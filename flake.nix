2008dff1a4fd16069c49ba1ff8e2a6d95b30303c
Evan Porter <evanwporter@gmail.com>
2026-08-28 20:40:20 -0700

    dev tooling

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "dwmblocks development workspace";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in
+    {
+      packages = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.stdenv.mkDerivation {
+            pname = "dwmblocks";
+            version = "unstable-2024-08-23";
+            src = self;
+
+            buildInputs = [ pkgs.libx11 ];
+            installFlags = [ "PREFIX=$(out)" ];
+          };
+        });
+
+      devShells = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            inputsFrom = [ self.packages.${system}.default ];
+            packages = with pkgs; [
+              bear
+              clang-tools
+              gnumake
+              jq
+            ];
+          };
+        });
+    };
+}
