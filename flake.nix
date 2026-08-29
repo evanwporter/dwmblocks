a364db46a986df56e57b141d78524e3cae90514b
Evan Porter <evanwporter@gmail.com>
2026-08-28 21:19:39 -0700

    format flake

--- flake.nix
+++ flake.nix
@@ -1,43 +1,44 @@
 {
-  description = "dwmblocks development workspace";
+	description = "dwmblocks development workspace";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+	inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-  outputs = { self, nixpkgs }:
-    let
-      supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
-      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-    in
-    {
-      packages = forAllSystems (system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-        in
-        {
-          default = pkgs.stdenv.mkDerivation {
-            pname = "dwmblocks";
-            version = "unstable-2024-08-23";
-            src = self;
+	outputs = {
+		self,
+		nixpkgs,
+	}: let
+		supportedSystems = ["x86_64-linux" "aarch64-linux"];
+		forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+	in {
+		packages =
+			forAllSystems (system: let
+					pkgs = nixpkgs.legacyPackages.${system};
+				in {
+					default =
+						pkgs.stdenv.mkDerivation {
+							pname = "dwmblocks";
+							version = "unstable-2024-08-23";
+							src = self;
 
-            buildInputs = [ pkgs.libx11 ];
-            installFlags = [ "PREFIX=$(out)" ];
-          };
-        });
+							buildInputs = [pkgs.libx11];
+							installFlags = ["PREFIX=$(out)"];
+						};
+				});
 
-      devShells = forAllSystems (system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-        in
-        {
-          default = pkgs.mkShell {
-            inputsFrom = [ self.packages.${system}.default ];
-            packages = with pkgs; [
-              bear
-              clang-tools
-              gnumake
-              jq
-            ];
-          };
-        });
-    };
+		devShells =
+			forAllSystems (system: let
+					pkgs = nixpkgs.legacyPackages.${system};
+				in {
+					default =
+						pkgs.mkShell {
+							inputsFrom = [self.packages.${system}.default];
+							packages = with pkgs; [
+								bear
+								clang-tools
+								gnumake
+								jq
+							];
+						};
+				});
+	};
 }
