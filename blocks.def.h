3eb0926438444317f6cd6c12d7b654ed0fe3da3e
Evan Porter <evanwporter@gmail.com>
2026-08-28 20:40:25 -0700

    custom blocks

--- blocks.def.h
+++ blocks.def.h
@@ -1,11 +1,13 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+    {"┇  ", "pactl get-sink-volume @DEFAULT_SINK@ | grep -o '[0-9]*%' | head -1", 2, 1},
+	{" ", "brightnessctl -m | cut -d, -f4",                                  2, 2},
+	{"󰁹 ", "printf '%s%%' \"$(cat /sys/class/power_supply/BAT0/capacity)\"",  15, 0},
+	{" ", "date '+%d.%m'",                                                  60, 0},
+	{" ", "date '+%H:%M  '",                                                 5, 0},
 };
 
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
-static char delim[] = " | ";
-static unsigned int delimLen = 5;
+static char delim[] = " ┇ ";
+static unsigned int delimLen = 7;
